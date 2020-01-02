e2793f849fd7f61ee4a82a2ab436ef737ee05e72
Alexsandro Saraiva <alex_saraiva14@hotmail.com>
2020-01-02 00:53:52 -0300

    criacao endpoint pedidos com as litas completas

---
+++ src/main/java/com/alexsaraiva/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.alexsaraiva.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexsaraiva.cursomc.domain.Pedido;
+import com.alexsaraiva.cursomc.repositories.PedidoRepository;
+import com.alexsaraiva.cursomc.services.exception.ObjectNotFoundException;
+
+
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository categoriaRepository;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = categoriaRepository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: "+id+" não disponivel. Tipo:"+ Pedido.class.getName()
+				));
+	}
+}
